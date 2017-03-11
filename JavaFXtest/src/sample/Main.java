71444b79e4a97f7462d6bcb68e30d857de62be61
IGrock88 <igrock88@gmail.com>
2017-03-11 16:31:25 +0800

    Create main class for calculator

---
+++ JavaFXtest/src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Calculator");
+        Scene scene = new Scene(root, 301, 295);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
